e934adce7afa5b27abf72ed9c3b88a934e1a0ccd
Kautilya03 <23ucs613@gmail.com>
2025-11-19 15:09:05 +0530

    Fixed Routes

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -45,7 +45,7 @@ router.get("/get-data", async (req, res) => {
 });
 
 // Route to get all medical histories of patient
-router.get("/get-medical-history", async (req, res) => {
+router.post("/get-medical-history", async (req, res) => {
   const patientId = req.body.patientId || req.query.patientId;
   if (!patientId) {
     return res.status(400).send("patientId query parameter is required");
@@ -81,7 +81,7 @@ router.get("/get-medical-history", async (req, res) => {
   }
 });
 
-router.get("/get-all-patients", async (req, res) => {
+router.post("/get-all-patients", async (req, res) => {
   try {
     const doctor=req.body.doctorId;
     const doctorData=await Doctor.findById(doctor).populate('accessToPatients');
